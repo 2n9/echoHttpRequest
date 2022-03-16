12659b70446fe77f99c7ea2182364d51ffa37743
a <a>
2022-03-16 15:40:37 +0900

    add base system

---
+++ main.dart
@@ -0,0 +1,17 @@
+#bool isInt(num value) => value is int || value == value.roundToDouble();
+bool isInt(String value) => s != null && (double.parse(s, (e) => null) != null);
+
+main(List<String> args) async {
+    int port = 8000;
+    if (args.length < 1) {
+        port = 3000;
+    } else {
+        if (isInt(args[0])) {
+            port = int.parse(args.[0]);
+        } else {
+            port = 3000;
+            print("please input number.");
+        }
+    }
+    print(port);
+}
